e58be5529491af459f3c2857833457ea03746b1b
Robby Dhindsa <dhindsa.1996@gmail.com>
2023-02-11 15:06:00 -0800

    add a comma to fix error

--- db/schema/01_schema.sql
+++ db/schema/01_schema.sql
@@ -28,7 +28,7 @@ CREATE TABLE conversations (
   id SERIAL PRIMARY KEY NOT NULL,
   time TIMESTAMP,
   message_content TEXT,
-  posting_id INTEGER REFERENCES postings(id) ON DELETE CASCADE
+  posting_id INTEGER REFERENCES postings(id) ON DELETE CASCADE,
 
   sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   recipient_id INTEGER REFERENCES users(id) ON DELETE CASCADE,

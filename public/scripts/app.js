4abc23a214ced8e100df1e195c30b61573c1f2fc
Brandie Roberts <brandiecroberts@gmail.com>
2023-02-14 16:24:13 -0800

    Add background image in header, create filter by price sliding scale

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,19 @@
 // Client facing scripts here
+
+
+//FILTER BY PRICE FUNCTIONALITY
+const minSlider = document.getElementById('min');
+const maxSlider = document.getElementById('max');
+const outputMin = document.getElementById('min-value');
+const outputMax = document.getElementById('max-value');
+
+outputMin.innerHTML = minSlider.value;
+outputMax.innerHTML = maxSlider.value;
+
+//SLIDER MOVES WITH MIN/MAX PRICE SHOWN
+minSlider.oninput = function() {
+  outputMin.innerHTML = this.value;
+};
+maxSlider.oninput = function() {
+  outputMax.innerHTML = this.value;
+};
